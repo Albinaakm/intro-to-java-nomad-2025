b73b0337ffe65830e8f47703a046081b3e830067
albina <albinaakm22@gmail.com>
2025-09-23 10:14:48 +0600

    feat: lab-1 test

---
+++ lab-01/src/task4.java
@@ -0,0 +1,18 @@
+//public class task4 {
+//    public static void main(String[] args) {
+//        System.out.println("a   a^2   a^3");
+//        System.out.println("1   " + (1*1) + "     " + (1*1*1));
+//        System.out.println("2   " + (2*2) + "     " + (2*2*2));
+//        System.out.println("3   " + (3*3) + "     " + (3*3*3));
+//        System.out.println("4   " + (4*4) + "    " + (4*4*4));
+//    }
+//}
+
+//public class task4 {
+//    public static void main(String[] args) {
+//        System.out.println("a   a^2  a^3");
+//        for (int a = 1; a <= 4; a++) {
+//            System.out.println(a + "   " + (a * a) + "    " + (a * a * a));
+//        }
+//    }
+//}
---
+++ lab-01/src/task5.java
@@ -0,0 +1,6 @@
+public class task5 {
+    public static void main(String[] args) {
+        double result = (9.5 * 4.5 - 2.5 * 3) / (45.5 - 3.5);
+        System.out.println("Result: " + result);
+    }
+}
